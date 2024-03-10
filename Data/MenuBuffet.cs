475437a3a25d5b2c94348e37ab125b6af33ee97c
LeTuan0210 <letuan1218414149@gmail.com>
2024-03-10 13:52:22 +0700

    Add project files.

---
+++ Data/MenuBuffet.cs
@@ -0,0 +1,11 @@
+﻿namespace SayakaWebApp.Data
+{
+    public class MenuBuffet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Price { get; set; } = 0;
+        public string Image {  get; set; } = string.Empty;
+    }
+}
